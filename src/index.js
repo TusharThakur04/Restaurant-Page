90f895fe011a05ff0ed74556a5626262103d2363
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2024-10-11 01:15:35 +0530

    modified:   src/index.js 	new file:   src/lasagna-5994612.jpg 	modified:   webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-const a = "welcome" ;
-console.log(a);
-alert(a);
+import rimage from "./lasagna-5994612.jpg" ;
+document.addEventListener("DOMContentLoaded", () => {
+
+const div = document.querySelector('#content') ;
+
+const head = document.createElement('div') ;
+head.className = "headline";
+head.textContent = "Welcome to Flavorscape: A Culinary Experience Like No Other!" ;
+const rName = "Flavorscape"
+const hText = head.textContent ;
+const newText = hText.replace(rName , `<span class = "highlight"> ${rName}</span>`);
+head.innerHTML=newText ;
+div.appendChild(head) ;
+
+const image = document.createElement('img') ;
+image.src = rimage ;
+div.appendChild(image);
+
+const desc = document.createElement('div') ;
+desc.className = "description" ;
+desc.textContent = "Welcome to Flavorscape, where every dish is a journey into the world of flavor and creativity. Our chefs are dedicated to blending traditional techniques with modern innovation, crafting dishes that are both visually stunning and irresistibly delicious. Whether you're in the mood for a light snack or a full-course meal, our menu offers a variety of options to satisfy every craving. Our atmosphere is designed to complement the culinary experience—inviting, elegant, and perfect for everything from intimate dinners to special celebrations. Join us for a memorable meal and savor the flavors that make us stand out."
+div.appendChild(desc);
+
+
+}) ;
+
+
+
