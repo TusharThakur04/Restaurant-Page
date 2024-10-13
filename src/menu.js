7848ec40410df93ee84683ef3adf7624cf0feac7
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2024-10-14 00:53:54 +0530

    new file:   src/menu.js

---
+++ src/menu.js
@@ -0,0 +1,9 @@
+export const menuload = ()=>{
+    document.addEventListener("DOMContentLoaded", () => {
+        const div = document.querySelector(#content);
+        const div2 = document.createElement('div');
+        div.className= 'menu';
+
+    })
+
+}
